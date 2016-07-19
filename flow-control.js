31f4896f4e1f60e75e1c0ba3db2ccf6551f247b9
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-19 15:34:00 -0400

    Done.

--- flow-control.js
+++ flow-control.js
@@ -33,26 +33,9 @@ function ternaryTeenager(age){
 }
 
 function switchAge(age){
-  switch(age) {
-    case 13: 
-      return 'You are a teenager'
-      break;
-    case 14: 
-      return 'You are a teenager'
-      break;
-    case 15: 
-      return 'You are a teenager'
-      break;
-    case 16: 
-      return 'You are a teenager'
-      break;
-    case 17: 
-      return 'You are a teenager'
-      break;
-    case 18: 
-      return 'You are a teenager'
-      break;
-    case 19: 
+  var x = age
+  switch(true) {
+    case (x <= 19 && x >=13): 
       return 'You are a teenager'
       break;
     
